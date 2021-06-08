ff7d85a9783eb9d9aa60885129d629f8cd52c594
Prakashgvs <prakashgvs789@gmail.com>
2021-06-08 20:29:53 +0530

    Addded universityDb Which contains Courses Table

---
+++ CleanArch/CleanArchInfra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchInfra.Data.Context
+{
+    class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Course> courses { get; set; }
+    }
+}
